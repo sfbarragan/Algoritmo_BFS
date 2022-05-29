72a08f468845a9160f8bf61414801d78beffef84
sfbarragan <sfbarragan@espe.edu.ec>
2022-05-29 12:52:22 -0500

    Se agrego el metodo agregar_borde de la clase Grafo

--- algoritmo_bfs.py
+++ algoritmo_bfs.py
@@ -54,7 +54,27 @@ class Grafo():
         m_lista_adyacencia : diccionario
             Diccionario que almacena el valor de los nodos
         '''
-        self.m_numero_nodos = numero_nodos
-        self.m_nodos = range(self.m_numero_nodos)
-        self.m_dirigido = dirigido
-        self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
+        self.m_numero_nodos = numero_nodos # Se asigna el valor del numero de nodos a través del parametro recibido
+        self.m_nodos = range(self.m_numero_nodos) #Se genera el rango de nodos en base a m_numero_nodos
+        self.m_dirigido = dirigido #se define el tipo de grafo 
+        self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos} #Se crea el diccionario de la lista de adyacencia
+
+    def agregar_borde(self, nodo1, nodo2, peso=1):
+        '''
+        Este método define el borde de la lista de adyacencia.
+        Recibe como parametros el nodo1, el nodo2 y el peso cuyo valor por defecto es de 1.
+        Posteriormente se agregan a la lista de adyacencia del nodo al que corresponde.
+        
+        Parametros
+        ----------
+        nodo1 : int
+        nodo2 : int
+        peso: int
+        
+        Retorno
+        -------
+        Nada 
+        '''
+        self.m_lista_adyacencia[nodo1].add((nodo2, peso))#Agrega el nodo 2 a la lista de adyacencia del nodo 1.
+        if not self.m_dirigido:
+            self.m_lista_adyacencia[nodo2].add((nodo1, peso))#Agrega el nodo 1 a la lista de adyacencia del nodo 2.
